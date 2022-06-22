00a530aeecdcf256ccbb8e2fbccfe41178cb1bf9
uttne <uttne.mkr@gmail.com>
2022-06-23 00:03:32 +0900

    MapControllers

--- src/AppTemplate/Program.cs
+++ src/AppTemplate/Program.cs
@@ -11,17 +11,17 @@ if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-
-app.UseHttpsRedirection();
+    
+    app.UseHttpsRedirection();
 }
 
 app.UseStaticFiles();
 app.UseRouting();
 
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
+app.MapControllers();
+// app.MapControllerRoute(
+//     name: "default",
+//     pattern: "{controller}/{action=Index}/{id?}");
 
 app.MapFallbackToFile("index.html");;
 
